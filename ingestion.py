31f08511361aef9bf8478136c90283b0c5989b43
m-ramzan-dev <mhdramzan.dev@gmail.com>
2026-06-25 18:52:42 +0500

    tavily crawl working

--- ingestion.py
+++ ingestion.py
@@ -1,52 +1,19 @@
-from langchain_community.document_loaders import PyPDFLoader
-from langchain_text_splitters import CharacterTextSplitter
+from dotenv import load_dotenv
+#from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_chroma import Chroma
+from langchain_core.documents import Document
 from langchain_openai import OpenAIEmbeddings
 from langchain_pinecone import PineconeVectorStore
-from dotenv import load_dotenv
-import os
+from langchain_tavily import TavilyCrawl, TavilyExtract, TavilyMap
 
 
-load_dotenv()
-
-from pinecone import Pinecone, ServerlessSpec
 
 def main():
-    print("ingestion!")
-    loader = PyPDFLoader(file_path="/Users/ramzan/Downloads/plum-networks-confidentiality-agreement_muhammad_ramzan.pdf")
-    documents = loader.load()
-    print("pdf file loaded")
-
-    text_splitter = CharacterTextSplitter(
-        separator="\n",
-        chunk_size=1000,
-        chunk_overlap=30
-    )
-
-    docs = text_splitter.split_documents(documents=documents)
-    print(f"document splited into {docs} chunks")
-
-    embeddings = OpenAIEmbeddings()
-    print("ingesting...")
-    index_name = os.environ.get('PINECONE_INDEX_NAME')
+    print("Ingestion for Documentation Helper!")
+    tavily_tool = TavilyCrawl()
+    tavily_tool_result = tavily_tool.invoke({"url":"https://khanquranacademy.com/"})
+    print(f"tavily_tool_result: {tavily_tool_result.get("results")}")
     
-    pc = Pinecone()
-    pc.create_index(
-        name=index_name,
-        dimension=1536,
-        metric="cosine",
-        spec=ServerlessSpec(cloud="aws", region="us-east-1"),
-    )
-    PineconeVectorStore.from_documents(
-        docs, embeddings,index_name=index_name
-    )
-    
-
-    print("finish")
-
-
-
-
-
 
 
 
